e84c75444bf3d5aa62f8e191cad6518ba24cc91d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-10 19:23:44 +0100

    custom mod update

--- worldmods/super_sam_custom/init.lua
+++ worldmods/super_sam_custom/init.lua
@@ -1,35 +1,4 @@
+local MP = minetest.get_modpath(minetest.get_current_modname())
 
--- emerge spawn area
-minetest.register_on_mods_loaded(function()
-    minetest.after(0, function()
-        -- first level
-        minetest.emerge_area({
-            x = -50,
-            y = -50,
-            z = -50
-        },{
-            x = 50,
-            y = 50,
-            z = 50
-        })
-
-        -- lounge
-        minetest.emerge_area({
-            x = 2050,
-            y = 0,
-            z = 0
-        },{
-            x = 1950,
-            y = 0,
-            z = 0
-        })
-    end)
-end)
-
--- periodically set a fixed time of day
-local function set_time()
-    minetest.set_timeofday(0.5)
-    minetest.after(2, set_time)
-end
-
-minetest.after(0, set_time)
+dofile(MP .. "/fixed_time.lua")
+dofile(MP .. "/update_check.lua")
---
+++ worldmods/super_sam_custom/update_check.lua
@@ -0,0 +1,21 @@
+
+local function get_version()
+    local f = assert(io.open(minetest.get_worldpath() .. "/gitversion", "r"))
+    local version = f:read("*all")
+    f:close()
+    return version
+end
+
+local start_version = get_version()
+
+local function check()
+    local current_version = get_version()
+    if current_version ~= start_version and #minetest.get_connected_players() == 0 then
+        -- version changed and no players online, restart
+        print("Version changed from " .. start_version .. " to " .. current_version .. " restarting server")
+        minetest.request_shutdown("map update", true)
+    end
+
+    minetest.after(10, check)
+end
+check()
