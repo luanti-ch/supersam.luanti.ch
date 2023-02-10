2c3e592118a7d0c9727f464094e13c5f203555cc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-10 13:15:20 +0100

    bridge / ui / other stuff

--- worldmods/super_sam_custom/init.lua
+++ worldmods/super_sam_custom/init.lua
@@ -1,10 +1,3 @@
--- restart if last player leaves
--- TODO: account for "inbound"/pre-auth players
-minetest.register_on_leaveplayer(function()
-    if #minetest.get_connected_players() == 1 then
-        minetest.request_shutdown("map reset", true)
-    end
-end)
 
 -- emerge spawn area
 minetest.register_on_mods_loaded(function()
