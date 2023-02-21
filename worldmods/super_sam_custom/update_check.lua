c7e3e26867cadae5c09672b40618b65bfaffe108
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-21 21:04:53 +0100

    icons

--- worldmods/super_sam_custom/update_check.lua
+++ worldmods/super_sam_custom/update_check.lua
@@ -21,17 +21,21 @@ end
 local start_version = get_version()
 local updated_since
 
+minetest.after(5, function()
+    chat_msg("➢ Server started with version '" .. start_version .. "'")
+end)
+
 local function check()
     local current_version = get_version()
     if not updated_since and current_version ~= start_version then
         -- mark update pending
         updated_since = os.time()
-        chat_msg("Update from '" .. start_version .. "' to '" .. current_version .. "' scheduled")
+        chat_msg("➢ Update from '" .. start_version .. "' to '" .. current_version .. "' scheduled")
     end
 
     if updated_since and (os.time() - updated_since) > 30 and #minetest.get_connected_players() == 0 then
         -- version changed a while ago and no players online, restart
-        chat_msg("Restarting to apply updates")
+        chat_msg("➢ Restarting to apply updates")
         minetest.request_shutdown("map update", true)
     end
 
