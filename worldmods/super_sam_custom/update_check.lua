0fcd3cb042ed8f09d01ff455ec8e8b7d0bb3514d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-21 20:56:25 +0100

    updated scheduling and deferred restart

--- worldmods/super_sam_custom/update_check.lua
+++ worldmods/super_sam_custom/update_check.lua
@@ -6,13 +6,32 @@ local function get_version()
     return version
 end
 
+-- generic chat message
+local function chat_msg(msg)
+    minetest.chat_send_all(msg)
+end
+
+-- beerchat message
+if minetest.get_modpath("beerchat") then
+    chat_msg = function(msg)
+        beerchat.on_channel_message("main", "SYSTEM", msg)
+    end
+end
+
 local start_version = get_version()
+local updated_since
 
 local function check()
     local current_version = get_version()
-    if current_version ~= start_version and #minetest.get_connected_players() == 0 then
-        -- version changed and no players online, restart
-        print("Version changed from " .. start_version .. " to " .. current_version .. " restarting server")
+    if not updated_since and current_version ~= start_version then
+        -- mark update pending
+        updated_since = os.time()
+        chat_msg("Update from '" .. start_version .. "' to '" .. current_version .. "' scheduled")
+    end
+
+    if updated_since and (os.time() - updated_since) > 30 and #minetest.get_connected_players() == 0 then
+        -- version changed a while ago and no players online, restart
+        chat_msg("Restarting to apply updates")
         minetest.request_shutdown("map update", true)
     end
 
