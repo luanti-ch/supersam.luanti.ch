69d9ffbf253c6dec7cc93e90914a3fad185f450e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-01-09 11:26:06 +0100

    remove embedded git modules

--- worldmods/super_sam_custom/fixed_time.lua
+++
@@ -1,5 +0,0 @@
-
--- set a fixed time of day
-minetest.after(5, function()
-    minetest.set_timeofday(0.5)
-end)
--- worldmods/super_sam_custom/level.lua
+++
@@ -1,7 +0,0 @@
-super_sam.on_event("level_finished", function(player, highscore_name, score, rank)
-    beerchat.on_channel_message("main", "SYSTEM", "➢ Player '" .. player:get_player_name() ..
-        "' finished level '" .. highscore_name ..
-        "' with " .. score ..
-        " points and rank " .. (rank and rank or "<none>")
-    )
-end)
